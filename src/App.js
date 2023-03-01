215b5d83cb3815b14684dbe49a949afe82dd48a3
anshuthopsee <79682606+anshuthopsee@users.noreply.github.com>
2023-03-01 03:37:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [grid, setGrid] = useState(null);
   const [currentPos, setCurrentPos] = useState([0, 0]);
   const [clickCount, setClickCount] = useState(0);
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   var maze;
 
   const generateClassName = (walls, r, c) => {
@@ -35,18 +36,51 @@ function App() {
   };
 
   const controls = (e) => {
-    e.preventDefault();
     if (currentPos[0] !== gridSize-1 || currentPos[1] !== gridSize-1) {
-      if (e.key === "ArrowLeft" || e.key === "a") setCurrentPos(() => [...maze.movePlayerPos(currentPos, "left")]);
-      if (e.key === "ArrowRight" || e.key === "d") setCurrentPos(() => [...maze.movePlayerPos(currentPos, "right")])
-      if (e.key === "ArrowUp" || e.key === "w") setCurrentPos(() => [...maze.movePlayerPos(currentPos, "top")]);
-      if (e.key === "ArrowDown" || e.key === "s") setCurrentPos(() => [...maze.movePlayerPos(currentPos, "bottom")]);
+      if (e.key === "ArrowLeft" || e.key === "a") {
+        e.preventDefault();
+        setCurrentPos(() => [...maze.movePlayerPos(currentPos, "left")]);
+      };
+      if (e.key === "ArrowRight" || e.key === "d") {
+        e.preventDefault();
+        setCurrentPos(() => [...maze.movePlayerPos(currentPos, "right")]);
+      };
+      if (e.key === "ArrowUp" || e.key === "w") {
+        e.preventDefault();
+        setCurrentPos(() => [...maze.movePlayerPos(currentPos, "top")]);
+      };
+      if (e.key === "ArrowDown" || e.key === "s") {
+        e.preventDefault();
+        setCurrentPos(() => [...maze.movePlayerPos(currentPos, "bottom")]);
+      };
     };
   };
 
   const showBanner = () => {
     if (currentPos[0] === gridSize-1 && currentPos[1] === gridSize-1) {
-      return <div className="banner" onClick={restartGame}>YOU WON! CLICK HERE TO PLAY AGAIN</div>
+      return <div style={{height: isMobile ? 50 : 100, fontSize: isMobile ? 20: 30}} className="banner" onClick={restartGame}>YOU WON! CLICK HERE TO PLAY AGAIN</div>
+    };
+  };
+
+  const triggerKeydown = (key) => {
+    const event = new KeyboardEvent('keydown', {
+      key: key
+    });
+    document.dispatchEvent(event);
+  };
+
+  const showControls = () => {
+    if (isMobile) {
+      return (
+        <div className="controls_container">
+          <div className="arrow up" onMouseDown={() => triggerKeydown("w")}><p>up</p></div>
+          <div className="left-right">
+            <div className="arrow left" onMouseDown={() => triggerKeydown("a")}><p>left</p></div>
+            <div className="arrow right" onMouseDown={() => triggerKeydown("d")}><p>right</p></div>
+          </div>
+          <div className="arrow down" onMouseDown={() => triggerKeydown("s")}><p>down</p></div>
+        </div>
+      );
     };
   };
 
@@ -56,6 +90,10 @@ function App() {
     setClickCount(clickCount+1)
   };
 
+  const maxLimit = () => {
+    return isMobile ? 13 : 40;
+  };
+
   useEffect(() => {
     maze = new Maze(gridSize);
     maze.setup();
@@ -63,24 +101,24 @@ function App() {
     console.log(maze.solveMaze());
 
     document.addEventListener('keydown', controls);
-
+    return () => document.removeEventListener("keydown", controls);
   }, [clickCount]);
 
   return (
     <>
         <div className="container">
           <div className="input_container">
-            <p>Maze Size (5-40)</p>
-            <input type="number" min={5} max={40} value={inputValue} onChange={(e) => {
-              if (e.target.value < 5 || e.target.value > 40) {
-              if (e.target.value-40 < e.target.value-5) {
-                setInputValue(40);
-              } else {
-                setInputValue(5);
+            <p>{isMobile ? "Use on-screen controls to play" : "Use WASD keys or Arrow keys to play"}</p>
+            <p>{`Maze Size (5-${maxLimit()})`}</p>
+            <input type="number" min={5} max={maxLimit()} name="maze-size" value={inputValue} onChange={(e) => {setInputValue(parseInt(e.target.value));}} onBlur={(e) => {
+              if (e.target.value < 5 || e.target.value > maxLimit()) {
+                if (Math.abs(e.target.value-maxLimit()) < Math.abs(e.target.value-5)) {
+                  setInputValue(maxLimit());
+                } else {
+                  setInputValue(5);
+                };
               };
-            }
-            setInputValue(e.target.value);
-          }}/>
+            }}/>
           <button onClick={restartGame}>Restart game</button>
         </div>
         <table style={{width: gridSize*35}}>
@@ -88,6 +126,7 @@ function App() {
             {drawMaze(5, 5)}
           </tbody>
         </table>
+        {showControls()}
       </div>
       {showBanner()}
     </>
